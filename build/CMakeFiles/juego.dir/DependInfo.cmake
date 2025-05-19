6d28cced9304cf61ba7a40fc98478e87322c9278
Isabel Adrada <isabel.adrada.a@gmail.com>
2025-05-19 04:49:34 +0000

    union archivos

---
+++ build/CMakeFiles/juego.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bel/proyectoFinal/Collider.cpp" "CMakeFiles/juego.dir/Collider.cpp.o" "gcc" "CMakeFiles/juego.dir/Collider.cpp.o.d"
+  "/home/bel/proyectoFinal/Escenario.cpp" "CMakeFiles/juego.dir/Escenario.cpp.o" "gcc" "CMakeFiles/juego.dir/Escenario.cpp.o.d"
+  "/home/bel/proyectoFinal/HormigaInfectada.cpp" "CMakeFiles/juego.dir/HormigaInfectada.cpp.o" "gcc" "CMakeFiles/juego.dir/HormigaInfectada.cpp.o.d"
+  "/home/bel/proyectoFinal/HormigaNPC.cpp" "CMakeFiles/juego.dir/HormigaNPC.cpp.o" "gcc" "CMakeFiles/juego.dir/HormigaNPC.cpp.o.d"
+  "/home/bel/proyectoFinal/HormigaNormal.cpp" "CMakeFiles/juego.dir/HormigaNormal.cpp.o" "gcc" "CMakeFiles/juego.dir/HormigaNormal.cpp.o.d"
+  "/home/bel/proyectoFinal/Juego.cpp" "CMakeFiles/juego.dir/Juego.cpp.o" "gcc" "CMakeFiles/juego.dir/Juego.cpp.o.d"
+  "/home/bel/proyectoFinal/Jugador.cpp" "CMakeFiles/juego.dir/Jugador.cpp.o" "gcc" "CMakeFiles/juego.dir/Jugador.cpp.o.d"
+  "/home/bel/proyectoFinal/Personaje.cpp" "CMakeFiles/juego.dir/Personaje.cpp.o" "gcc" "CMakeFiles/juego.dir/Personaje.cpp.o.d"
+  "/home/bel/proyectoFinal/Ray.cpp" "CMakeFiles/juego.dir/Ray.cpp.o" "gcc" "CMakeFiles/juego.dir/Ray.cpp.o.d"
+  "/home/bel/proyectoFinal/ReyHongo.cpp" "CMakeFiles/juego.dir/ReyHongo.cpp.o" "gcc" "CMakeFiles/juego.dir/ReyHongo.cpp.o.d"
+  "/home/bel/proyectoFinal/main.cpp" "CMakeFiles/juego.dir/main.cpp.o" "gcc" "CMakeFiles/juego.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/juego.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/juego.dir/Collider.cpp.o"
+  "CMakeFiles/juego.dir/Collider.cpp.o.d"
+  "CMakeFiles/juego.dir/Escenario.cpp.o"
+  "CMakeFiles/juego.dir/Escenario.cpp.o.d"
+  "CMakeFiles/juego.dir/HormigaInfectada.cpp.o"
+  "CMakeFiles/juego.dir/HormigaInfectada.cpp.o.d"
+  "CMakeFiles/juego.dir/HormigaNPC.cpp.o"
+  "CMakeFiles/juego.dir/HormigaNPC.cpp.o.d"
+  "CMakeFiles/juego.dir/HormigaNormal.cpp.o"
+  "CMakeFiles/juego.dir/HormigaNormal.cpp.o.d"
+  "CMakeFiles/juego.dir/Juego.cpp.o"
+  "CMakeFiles/juego.dir/Juego.cpp.o.d"
+  "CMakeFiles/juego.dir/Jugador.cpp.o"
+  "CMakeFiles/juego.dir/Jugador.cpp.o.d"
+  "CMakeFiles/juego.dir/Personaje.cpp.o"
+  "CMakeFiles/juego.dir/Personaje.cpp.o.d"
+  "CMakeFiles/juego.dir/Ray.cpp.o"
+  "CMakeFiles/juego.dir/Ray.cpp.o.d"
+  "CMakeFiles/juego.dir/ReyHongo.cpp.o"
+  "CMakeFiles/juego.dir/ReyHongo.cpp.o.d"
+  "CMakeFiles/juego.dir/main.cpp.o"
+  "CMakeFiles/juego.dir/main.cpp.o.d"
+  "juego"
+  "juego.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/juego.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
